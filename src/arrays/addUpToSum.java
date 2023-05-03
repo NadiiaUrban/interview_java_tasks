b9fe5ae5ff640ab32f0bd0f8ce020ece87989621
Nadiia <nadiiaurban@gmail.com>
2023-05-03 11:25:22 -0600

    created package Array with class addUpToSum

---
+++ src/arrays/addUpToSum.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class addUpToSum {
+}
+
+/*
+     given an int[] 'arr' and another int 'sum',
+     Write a function which can find a pair of ints in 'arr' that add up to 'sum'.
+        Example:
+            arr = [8, 7, 2, 5, 3, 1]
+            sum = 10
+            Output:
+                {8=2, 7=3}
+     */
